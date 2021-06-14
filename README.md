1241e338082d4883af74f8cfda74b06f648ec3a3
Sriyans Reddy <ysriyansreddy@gmail.com>
2021-06-14 12:21:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pet-nibbles
+ 
---
+++ index.js
@@ -0,0 +1,13 @@
+// 🐱 🐶 🐇 
+
+document.getElementById("cat-signup").addEventListener("click", function() {
+  document.getElementById("cat-price-main").style.color = "#ffd60a"
+})
+
+document.getElementById("dog-signup").addEventListener("click", function() {
+  document.getElementById("dog-price-main").style.color = "#ffd60a"
+})
+
+document.getElementById("rabbit-signup").addEventListener("click", function() {
+  document.getElementById("rabbit-price-main").style.color = "#ffd60a"
+})
